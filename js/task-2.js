707e3a0f62155a60ebc86763b5afb2b251195d2e
Vitalii Zaidenshnir <sapsan281993@gmail.com>
2024-12-16 19:28:13 +0100

    2

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+function makeArray(firstArray, secondArray, maxLength){
+    let finishMas = firstArray.concat(secondArray);
+    let numberMas = finishMas.length;
+    if (numberMas <= maxLength){
+        return finishMas;
+    } else {
+        return finishMas.splice(0, maxLength)
+    }
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
